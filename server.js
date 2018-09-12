c346ccbbd45d0f76157dcfef82d663d9831067d1
Elizabeth P <ejpor@DESKTOP-A373663>
2018-09-12 10:34:59 -0400

    server

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Database configuration
 const databaseUrl = "LACE";
-const collections = ["Bills"];
+const collections = ["Hansard"];
 
 // Hook mongojs configuration to the db variable
 const db = mongojs(databaseUrl, collections);
@@ -25,32 +25,23 @@ app.get("/", (req, res) => {
 
 
 app.get("/scrape", (req, res) => {
-  
-  request("https://www.ola.org/en/legislative-business/bills/current", (error, response, html) => {
-    
+
+  request("https://www.ola.org/en/legislative-business/house-documents/parliament-42/session-1/2018-08-14/hansard#P402_92152", (error, response, html) => {
+
     let $ = cheerio.load(html);
     cheerioTableParser($)
-    
-    $(".views-row").each(function(i, element) {
+
+    $(".speakerStart").each((i, element) => {
+
+      let object = $(element).find("strong").text();
+      // var ford = obj.map(name => name.id);
+           
+     
+        console.log(object);
       
-      
-      let title = $(element).find("h2").text();
-      let URL = $(element).find("a").attr("href");
-      let MPP = $(element).find("p").text();
-      let data = $(element).parsetable(false, false, true);
-  
-      // let date = $(element).find("td").eq(0).text()
-      // let billStage = $(element).find("td").eq(1).text()
-      // let activity =  $(element).find("td").eq(2).text()
-      // let committee =  $(element).find("td").eq(3).text()
-      
-      // If this found element had both a title and a link
-      if (title && URL) {
-        // Insert the data in the scrapedData db
-        db.scrapedData.insert({
-          title, URL, MPP, data
-          // date, billStage, activity, committee
-        },
+      db.Hansard.insert({
+        object
+      },
         (err, inserted) => {
           if (err) {
             // Log the error if one is encountered during the query
@@ -58,14 +49,52 @@ app.get("/scrape", (req, res) => {
           }
           else {
             // Otherwise, log the inserted data
-            console.log(inserted)
           }
+          // });
         });
-      }
     });
   });
 
-  // Send a "Scrape Complete" message to the browser
+  //   request("https://www.ola.org/en/legislative-business/bills/current", (error, response, html) => {
+
+  //     let $ = cheerio.load(html);
+  //     cheerioTableParser($)
+
+  //     $(".views-row").each((i, element) => {
+
+
+  //       let title = $(element).find("h2").text();
+  //       let URL = $(element).find("a").attr("href");
+  //       let MPP = $(element).find("p").text();
+  //       let data = $(element).parsetable(false, false, true);
+
+  //       // let date = $(element).find("td").eq(0).text()
+  //       // let billStage = $(element).find("td").eq(1).text()
+  //       // let activity =  $(element).find("td").eq(2).text()
+  //       // let committee =  $(element).find("td").eq(3).text()
+
+  //       // If this found element had both a title and a link
+  //       if (title && URL) {
+  //         // Insert the data in the scrapedData db
+  //         db.scrapedData.insert({
+  //           title, URL, MPP, data
+  //           // date, billStage, activity, committee
+  //         },
+  //         (err, inserted) => {
+  //           if (err) {
+  //             // Log the error if one is encountered during the query
+  //             console.log(err);
+  //           }
+  //           else {
+  //             // Otherwise, log the inserted data
+  //             console.log(inserted)
+  //           }
+  //         });
+  //       }
+  //     });
+  //   });
+
+  //Send a "Scrape Complete" message to the browser
   res.send("Scrape Complete");
 });
 
