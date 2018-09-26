d2bcfc01eb3db72d9e6a9826e2abe55375cb7cd6
Christine <aqui.christine@gmail.com>
2018-09-26 01:11:17 -0400

    added basic navbar with routing

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,12 @@ class Navbar extends Component {
 
   render(){
     return (
-      <h1>Hello Navbar !</h1>
+      <nav class="pa3 pa4-ns">
+        <Link to={'/'}  class="link dim black b f6 f5-ns dib mr3" href="#" title="Home">Site Name</Link>
+        <Link to={'/signup'}  class="link dim gray    f6 f5-ns dib mr3" href="#" title="signup">Sign Up</Link>
+        <Link to={'/login'}  class="link dim gray    f6 f5-ns dib mr3" href="#" title="login">Login</Link>
+        <Link to={'/profile'}  class="link dim gray    f6 f5-ns dib mr3" href="#" title="other">other?</Link>
+      </nav>
     )
   }
 }
