688d3389db10f38fade356b131ecc5162ce07755
Chris <aqui.christine@gmail.com>
2018-09-29 09:35:10 -0400

    working on selected mpp with mockup data

--- client/src/components/dashComponents/Mppinfo.js
+++ client/src/components/dashComponents/Mppinfo.js
@@ -1,41 +1,42 @@
-import React, { Component } from 'react';
-
+import React from 'react';
 
 const styles = {
   layout: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'center',
-    alignItem: 'center'
+    alignItem: 'center',
   },
-  fixedHeight:{
-    height: '280px'
-  }
-}
+  fixedHeight: {
+    height: '280px',
+  },
+};
 
 const Mppinfo = props => (
-  <div className='container center' style={styles.layout} >
-
-    <article style={styles.fixedHeight}  className="mw5 bg-white br3 pa3 pa4-ns ba b--black-10">
+  <div className="container center" style={styles.layout}>
+    <article style={styles.fixedHeight} className="mw5 bg-white br3 pa3 pa4-ns ba b--black-10">
       <div className="tc">
-        <img src="http://tachyons.io/img/avatar_1.jpg" className="br-100 h4 w4 dib ba b--black-05 pa2" alt='selected MPP'></img>
+        <img
+          src={props.picture}
+          className="br-100 h4 w4 dib ba b--black-05 pa2"
+          alt="selected MPP"
+        />
         <h1 className="f3 mb2">Ford something</h1>
         <h2 className="f5 fw4 gray mt0">Position: something</h2>
       </div>
     </article>
 
     <article style={styles.fixedHeight} className="br3 hidden ba b--black-10">
-      <h1 className="f4 bg-near-white br3 br--top black-60 mv0 pv2 ph3">Information on "MPP Name here"</h1>
+      <h1 className="f4 bg-near-white br3 br--top black-60 mv0 pv2 ph3">Information on {props.name}</h1>
       <div className="pa3 bt b--black-10">
-        <h3>Name:{props.name}</h3>
-        <h3>Current Position: {props.postion}</h3>
-        <h3>Contact number: {props.number}</h3>
-        <h3></h3>
-        <h3></h3>
-        <h3></h3>
+        <p>Name:<strong>{props.name}</strong> </p>
+        <p>Current Position:<strong>{props.position}</strong> </p>
+        <p>Contact number:<strong>{props.number}</strong> </p>
+        <p />
+        <h3 />
+        <h3 />
       </div>
     </article>
-
   </div>
 );
 
