a14005e2e41fd091f57a0e84908f9d81daf87375
ela-p-p <35507485+ela-p-p@users.noreply.github.com>
2018-09-30 23:44:46 -0400

    Update Bills.js

--- client/database/models/Bills.js
+++ client/database/models/Bills.js
@@ -1,32 +1,32 @@
-import * as mongoose from 'mongoose';
+// import * as mongoose from 'mongoose';
 
-// Save a reference to the Schema constructor
-const Schema = mongoose.Schema;
+// // Save a reference to the Schema constructor
+// const Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
-const BillsSchema = new Schema({
-  // `title` is required and of type String
-  title: {
-    type: String,
-    required: true,
-  },
-  URL: {
-    type: String,
-    required: true,
-  },
-  MPP: {
-    type: String,
-    required: true,
-  },
-  data: {
-    type: String,
-    required: true,
-  },
-});
+// // Using the Schema constructor, create a new UserSchema object
+// // This is similar to a Sequelize model
+// const BillsSchema = new Schema({
+//   // `title` is required and of type String
+//   title: {
+//     type: String,
+//     required: true,
+//   },
+//   URL: {
+//     type: String,
+//     required: true,
+//   },
+//   MPP: {
+//     type: String,
+//     required: true,
+//   },
+//   data: {
+//     type: String,
+//     required: true,
+//   },
+// });
 
-// This creates our model from the above schema, using mongoose's model method
-const Bills = mongoose.model('Bills', BillsSchema);
+// // This creates our model from the above schema, using mongoose's model method
+// const Bills = mongoose.model('Bills', BillsSchema);
 
-// Export the Article model
-export default Bills;
+// // Export the Article model
+// export default Bills;
