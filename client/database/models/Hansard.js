10ec86a2532302729c35eb9b5291dcb78e4f2f57
ela-p-p <35507485+ela-p-p@users.noreply.github.com>
2018-09-30 23:45:07 -0400

    Update Hansard.js

--- client/database/models/Hansard.js
+++ client/database/models/Hansard.js
@@ -1,19 +1,19 @@
-import * as mongoose from 'mongoose';
+// import * as mongoose from 'mongoose';
 
-// Save a reference to the Schema constructor
-const Schema = mongoose.Schema;
+// // Save a reference to the Schema constructor
+// const Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
-const HansardSchema = new Schema({
-  // `title` is required and of type String
-  object: {
-    type: String,
-  },
-});
+// // Using the Schema constructor, create a new UserSchema object
+// // This is similar to a Sequelize model
+// const HansardSchema = new Schema({
+//   // `title` is required and of type String
+//   object: {
+//     type: String,
+//   },
+// });
 
-// This creates our model from the above schema, using mongoose's model method
-const Hansard = mongoose.model('Hansard', HansardSchema);
+// // This creates our model from the above schema, using mongoose's model method
+// const Hansard = mongoose.model('Hansard', HansardSchema);
 
-// Export the Article model
-export default Hansard;
+// // Export the Article model
+// export default Hansard;
