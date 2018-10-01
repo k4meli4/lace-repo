c519fc9263c1fa0f15f5468ea2abc130edce76c0
ela-p-p <35507485+ela-p-p@users.noreply.github.com>
2018-09-30 23:47:16 -0400

    Update Bills.js

--- client/database/models/Bills.js
+++ client/database/models/Bills.js
@@ -3,8 +3,6 @@
 // // Save a reference to the Schema constructor
 // const Schema = mongoose.Schema;
 
-// // Using the Schema constructor, create a new UserSchema object
-// // This is similar to a Sequelize model
 // const BillsSchema = new Schema({
 //   // `title` is required and of type String
 //   title: {
@@ -28,5 +26,4 @@
 // // This creates our model from the above schema, using mongoose's model method
 // const Bills = mongoose.model('Bills', BillsSchema);
 
-// // Export the Article model
 // export default Bills;
