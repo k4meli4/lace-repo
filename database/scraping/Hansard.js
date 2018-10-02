e8d468c3958fad9969e091b369a53561858c4002
ela-p-p <ejporter9@hotmail.com>
2018-10-01 20:07:25 -0400

    databaseRoutes

---
+++ database/scraping/Hansard.js
@@ -0,0 +1,41 @@
+const cheerio = require('cheerio');
+const axios = require('axios');
+const mongoose = require('mongoose');
+const Hansard = require('../models/Hansard');
+
+mongoose.connect('mongodb://localhost/lace-repo');
+// still working on an array to get each page, right now the scrape is hardcoded with Dougie.
+const hansardRouter = router => {
+  router.get('/scrape', (_req, res) => {
+    axios
+      .get(
+        'https://www.ola.org/en/legislative-business/house-documents/parliament-42/session-1/2018-08-14/hansard#P402_92152'
+      )
+      .then(response => {
+        const $ = cheerio.load(response.data);
+
+        $('.speakerStart').each((_i, element) => {
+          const result = {};
+
+          result.object = $(element).text();
+
+          console.log(result);
+
+          Hansard.create(result)
+            .then(dbHansard => {
+              // View the added result in the console
+              console.log(dbHansard);
+            })
+            .catch(err =>
+              // If an error occurred, send it to the client
+              res.json(err)
+            );
+        });
+      });
+
+    // Send a "Scrape Complete" message to the browser
+    res.send('Scrape Complete');
+  });
+  return router;
+};
+module.exports = hansardRouter;
--- server.js
+++ server.js
@@ -2,18 +2,12 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-// const mongojs = require('mongojs');
-// const request = require('request');
-const cheerio = require('cheerio');
-const axios = require('axios');
-// const cheerioTableParser = require('cheerio-tableparser');
 const mongoose = require('mongoose');
-// const db = require('./client/database/models');
+// const db = require('./database/models');
 
 // Initialize Express
 const app = express();
 const PORT = process.env.PORT || 8080;
-
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -21,54 +15,38 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
+let billsRouter = express.Router();
+billsRouter = require('./database/scraping/Bills')(billsRouter);
+
+let eachMPPRouter = express.Router();
+eachMPPRouter = require('./database/scraping/eachMPP')(eachMPPRouter);
+
+let mppUrlRouter = express.Router();
+mppUrlRouter = require('./database/scraping/MPPurls')(mppUrlRouter);
+
+let hansardRouter = express.Router();
+hansardRouter = require('./database/scraping/Hansard')(hansardRouter);
+
+app.use('/bills', billsRouter);
+app.use('/eachMPP', eachMPPRouter);
+app.use('/mppUrl', mppUrlRouter);
+app.use('/hansard', hansardRouter);
 mongoose.connect('mongodb://localhost/lace-repo');
 
 // database is called lace-repo, you can see from 'mongoose.connect' code above
 // Scraping steps:
-// **uncomment lines 5-11, sorry issues with es6 and passing the build
+// **uncomment all the db files, sorry es6 compile issues (will ask Uzair)
 // 1) connect to your mongoDB
-// 2) open Database folder, open scraping folder, copy code from "scrape" file
-// 3) save
-// 4) yarn start (this is development start, uses nodemon)
-// 5) let React app load (loads on 3000)
-// 6) open a new tab in your browser and type http://localhost:8080/scrape in browser
-// 7) you'll get a "Scrape Complete" if things worked
-// 8) come back to this page and you will see results in the console.
-// 9) view collection in MongoDB in the lace-repo database
+// 2) yarn start (this is development start, uses nodemon)
+// 3) let React app load (loads on 3000)
+// 4) open a new tab in your browser
+// 5a) to scrape bills: type http://localhost:8080/bills/scrape in browser
+// 6) you'll get a "Scrape Complete" if things worked
+// 7) must scrape mppUrl before each MPP. To scrape mppurl: type http://localhost:8080/mppUrl/scrape in browser
+// 8) remidner, scrape mppUrl first. To scrape eachMPP: type http://localhost:8080/eachMPP/scrape in browser
+// 9) to scrape hansard: type http://localhost:8080/hansard/scrape in browser
+// 10) view collection in MongoDB in the lace-repo database
 
-// copy Entire scrape file just below here
-// HERE//
-app.get('/scrape', (_req, res) => {
-  axios
-    .get(
-      'https://www.ola.org/en/legislative-business/house-documents/parliament-42/session-1/2018-08-14/hansard#P402_92152'
-    )
-    .then(response => {
-      const $ = cheerio.load(response.data);
-
-      $('.speakerStart').each((_i, element) => {
-        const result = {};
-
-        result.object = $(element).text();
-
-        console.log(result);
-
-        //         db.Hansard.create(result)
-        //           .then(dbHansard => {
-        //             // View the added result in the console
-        //             console.log(dbHansard);
-        //          })
-        //          .catch(err =>
-        // If an error occurred, send it to the client
-        //            res.json(err)
-        //          );
-      });
-    });
-
-  // Send a "Scrape Complete" message to the browser
-  res.send('Scrape Complete');
-});
-// scrape code goes ABOVE here!!
 app.get('*', (_req, res) => {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
