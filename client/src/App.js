052ecc8be573d317b172a9a74f0c095f42f4bd67
Chris <aqui.christine@gmail.com>
2018-10-02 19:29:24 -0400

    added the the view for login and signup just to demo but not functional

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Landing from './components/Landing';
 import SelectedDash from './components/SelectedDash';
+import Login from './components/Login';
+import Signup from './components/Signup';
 // styling
 import './App.css';
 
@@ -13,14 +15,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
       <BrowserRouter>
         <div>
         <Navbar />
           <Route exact path="/" component={Landing} />
           <Route exact path="/test" component={SelectedDash} />
-          <Route exact path="/login" component={SelectedDash} />
-          <Route exact path="/signup" component={SelectedDash} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
         </div>
       </BrowserRouter>
       </div>
---
+++ client/src/components/Login.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="pa4 black-80">
+        <form className="measure center">
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className="f4 fw6 ph0 mh0">Sign In</legend>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+              <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"></input>
+            </div>
+            <div className="mv3">
+              <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+              <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"></input>
+            </div>
+            <label className="pa0 ma0 lh-copy f6 pointer"><input type="checkbox"/>Remember me</label>
+          </fieldset>
+          <div className="">
+            <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in"></input>
+          </div>
+          <div className="lh-copy mt3">
+            <a href="#0" className="f6 link dim black db">Sign up</a>
+            <a href="#0" className="f6 link dim black db">Forgot your password?</a>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Login;
