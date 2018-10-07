ceadd7ce32543f3ff7e90b668a1610072988c142
Chris <aqui.christine@gmail.com>
2018-10-07 14:37:53 -0400

    milestone

--- server.js
+++ server.js
@@ -59,12 +59,16 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback',
     },
-    accessToken => {
-      console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token', accessToken);
+      console.log('refresh Token', refreshToken);
+      console.log('profile ', profile);
+      console.log(done);
     }
   )
 );
 
+// PASSPORT STUFF
 // this router handler will try to auth the user using passport and passing the passport strategy (defined above as googleStrategy} - CA notes
 // code internal to googel oauth20 has some logic that thats authentics with a string of google will use this GoogleStrategy. -CA notes
 // the scope is asking google to give access to the user profile and email. this is defined types, see docs for more -CA notes
@@ -74,6 +78,10 @@ app.get(
     scope: ['profile', 'email'],
   })
 );
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+// END OF PASSPORT STUFF
 
 app.get('*', (_req, res) => {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
