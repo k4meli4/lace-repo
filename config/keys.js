c5f83833c0f2b51e821c97c6ebc0af59829c189b
Chris <aqui.christine@gmail.com>
2018-10-07 23:40:32 -0400

    env change

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+if (process.env.NODE_ENV === 'production') {
+  //  we are in production return th prod keys
+  module.exports = require('./prod');
+} else {
+  // we are in dev and return the dev keys
+  module.exports = require('./dev');
+}
