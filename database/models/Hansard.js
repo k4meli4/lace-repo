544913a87970dec56935d9b7eef7fa5fe581a894
ela-p-p <ejporter9@hotmail.com>
2018-10-08 12:24:25 -0400

    hansard

--- database/models/Hansard.js
+++ database/models/Hansard.js
@@ -3,13 +3,14 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const HansardSchema = Schema({
+const HansardSchema = new Schema({
   name: {
-    type: String,
+    type: String
   },
   speech: {
+    type: String
+  }
 });
-
 const Hansard = mongoose.model('Hansard', HansardSchema);
 
 module.exports = Hansard;
