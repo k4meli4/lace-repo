357657219e93d93fc8af1f3663efbed0fbb4af4e
Chris <aqui.christine@gmail.com>
2018-10-07 21:59:09 -0400

    :construction: serializeUser and deserializeUser passport user function. next step- tell passport to use COOKIES
     #82

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,25 @@ const keys = require('../config/keys');
 const mongoose = require('mongoose');
 
 const User = mongoose.model('users');
+//
+
+//  this user here is not the same user as defined above.
+// this user is the user data retuend from google in the promise
+passport.serializeUser( (user, done) => {
+  done(null, user.id);// done is a call back, null is a err that is passed in to check if everything is fine. user.id will id the user in FOLLOW UP REQUEST.
+  // this user id is th id that is assigned by mongo and not google.
+
+  // here we turn a mongoose model instance into an id
+});
+
+passport.deserializeUser( (id, done) => {
+  User.findById(id)
+  .then(user => {
+    done(null, user);
+  })
+
+  //  here we turn an id into a mongoose model instance.
+});
 
 //  google oauth stuff
 // '/auth/google/callback' is the callback route being used for handle user data coming back from google
@@ -18,12 +37,16 @@ passport.use(
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id })
-      .then((existingUser)=> {
+      .then(existingUser => {
         if (existingUser){
           // user is already in db
+          comsole.log('This user has signed in with this id before. No New db record');
+          done(null, existingUser);
         } else {
           // user is not in db
-          new User({ googleId: profile.id }).save();
+          new User({ googleId: profile.id })
+          .save()
+          .then(user => done(null, user));
         }
       })
       // console.log('access token', accessToken);
