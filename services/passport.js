78b2472337e0b38476fe882e8bcc1049e4698c36
Chris <aqui.christine@gmail.com>
2018-10-07 21:28:59 -0400

    added condition to check is user is already in db

--- services/passport.js
+++ services/passport.js
@@ -17,7 +17,15 @@ passport.use(
       callbackURL: '/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id })
+      .then((existingUser)=> {
+        if (existingUser){
+          // user is already in db
+        } else {
+          // user is not in db
+          new User({ googleId: profile.id }).save();
+        }
+      })
       // console.log('access token', accessToken);
       // console.log('refresh Token', refreshToken);
       // console.log('profile ', profile);
