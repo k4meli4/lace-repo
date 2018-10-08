fa73c37b115ad9f0c39f13341cf19faf514d4437
Chris <aqui.christine@gmail.com>
2018-10-07 22:57:21 -0400

    added route to get current user

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,6 +14,11 @@ module.exports = app => {
   );
 
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  //
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
 };
 
 // END OF PASSPORT STUFF
