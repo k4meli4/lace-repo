4d916371bc8f65d0ff64619a289dbf7b89ec244e
Chris <aqui.christine@gmail.com>
2018-10-10 23:17:14 -0400

    one more

--- client/src/components/dashComponents/SocialFeed.js
+++ client/src/components/dashComponents/SocialFeed.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const styles ={
+const styles = {
   layout: {
     height: '500px',
     'overflow-x': 'hidden',
     'overflow-y': 'scroll',
-  }
-}
+  },
+};
 
 const SocialFeed = () => (
   <article id="" style={styles.layout} className="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
@@ -15,7 +15,10 @@ const SocialFeed = () => (
       <hr className="mw3 bb bw1 b--black-10" />
     </div>
     <div className="lh-copy measure center f6 black-70">
-      <a className="twitter-timeline" href="https://twitter.com/fordnation?lang=en">Tweets by TwitterDev</a> <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+      <a className="twitter-timeline" href="https://twitter.com/fordnation?lang=en">
+        Tweets by TwitterDev
+      </a>{' '}
+      <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8" />
     </div>
   </article>
 );
