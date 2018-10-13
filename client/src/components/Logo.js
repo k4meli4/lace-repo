a4b5ba68557b959212481df2baa03582b8ec0ba3
Chris <aqui.christine@gmail.com>
2018-10-13 00:26:38 -0400

    checkpoint

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
 import logo from '../images/logo.jpg';
 
-
 // const tolanding = setTimeout(() => window.location +'/test', 5000);
-
 
 class Logo extends Component {
   // let tolanding = window.location +'/test'
   componentDidUpdate() {
-    console.log('its mounted')
+    console.log('its mounted');
     setTimeout(() => {
-      console.log('this logo timer')
+      console.log('this logo timer');
       window.location = '/test';
     }, 2000);
   }
 
-  render(){
-    return(
-    <img src={logo} alt="Logo" />
-    )
+  render() {
+    return <img src={logo} alt="Logo" />;
   }
-};
+}
 
 export default Logo;
