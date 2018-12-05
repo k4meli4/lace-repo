78c4fcec451996e9d4ab6a06d6cb8d8616cfecbe
christine-aqui <aqui.christine@gmail.com>
2018-12-05 18:29:09 -0500

    name back

--- client/src/components/list/mppSocial.js
+++ client/src/components/list/mppSocial.js
@@ -2,53 +2,53 @@ const demoList = [
   {
     name: 'Hon. Doug Ford',
     facebook: 'https://www.facebook.com/FordNationDougFord/',
-    twitter: 'https://twitter.com/fordnation?lang=en',
+    twitter: 'https://twitter.com/fordnation?lang=en'
   },
   {
     name: 'Kathleen O. Wynne',
     facebook: 'https://www.facebook.com/WynneFans',
-    twitter: 'https://twitter.com/kathleen_wynne',
+    twitter: 'https://twitter.com/kathleen_wynne'
   },
   {
     name: 'Andrea Horwath',
     facebook: 'https://www.facebook.com/AndreaHorwathONDP',
-    twitter: 'https://twitter.com/andreahorwath',
+    twitter: 'https://twitter.com/andreahorwath'
   },
   {
     name: 'Michael Coteau',
     facebook: 'https://www.facebook.com/coteaumichael/',
-    twitter: 'https://twitter.com/coteau',
+    twitter: 'https://twitter.com/coteau'
   },
   {
     name: 'Hon. Sylvia Jones',
     facebook: 'https://www.facebook.com/sylviavjones',
-    twitter: 'https://twitter.com/sylviajonesmpp',
+    twitter: 'https://twitter.com/sylviajonesmpp'
   },
   {
     name: 'Mike Schreiner',
     facebook: 'https://www.facebook.com/mike.schreiner/',
-    twitter: 'https://twitter.com/mikeschreiner',
+    twitter: 'https://twitter.com/mikeschreiner'
   },
   {
     name: 'Gilles Bisson',
     facebook: 'https://www.facebook.com/GillesBissonONDP',
-    twitter: 'https://twitter.com/bissongilles',
+    twitter: 'https://twitter.com/bissongilles'
   },
   {
     name: 'Hon. Christine Elliott',
     facebook: 'https://www.facebook.com/ChristineElliottON/',
-    twitter: 'https://twitter.com/celliottability',
+    twitter: 'https://twitter.com/celliottability'
   },
   {
     name: 'John Fraser',
     facebook: 'https://www.facebook.com/JohnFraserOttawaSouth',
-    twitter: 'https://twitter.com/johnfraseros?lang=en',
+    twitter: 'https://twitter.com/johnfraseros?lang=en'
   },
   {
     name: 'Jessica Bell',
     facebook: 'https://www.facebook.com/UniRoseJessicaBell',
-    twitter: 'https://twitter.com/jessicabellto?lang=en',
-  },
+    twitter: 'https://twitter.com/jessicabellto?lang=en'
+  }
 ];
 
 export default demoList;
